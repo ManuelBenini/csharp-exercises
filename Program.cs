a8a946beece7ec67163c93f2cf89655499375a82
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-16 14:44:39 +0200

    Snack 4 terminato

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
 
 */
 
+/*
+
 Console.WriteLine("Inserire primo numero");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
@@ -20,6 +22,9 @@ else
 {
     Console.WriteLine($"Il numero più grande è {secondNumber}, il più piccolo è {firstNumber}");
 }
+
+*/
+
 #endregion
 
 #region SNACK 2
@@ -29,6 +34,8 @@ else
     L'utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la più lunga.
 
 */
+
+/*
 
 Console.WriteLine("Inserire prima parola");
 string firstWord = Console.ReadLine();
@@ -44,4 +51,57 @@ else
 {
     Console.WriteLine($"La parola più corta è {firstWord}, la più lunga è {secondWord}");
 }
+
+*/
+
+#endregion
+
+#region SNACK 3
+
+/* Consegna
+
+    Il software deve chiedere per 10 volte all'utente di inserire un numero.
+    Il programma stampa la somma di tutti i numeri inseriti.
+
+*/
+
+/*
+
+int n = 10;
+int sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Inserire un numero. Numeri rimanenti: {n - i}");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    sum = sum + number;
+}
+
+Console.WriteLine($"La somma di tutti i numeri inseriti è: {sum}");
+
+*/
+
+#endregion
+
+#region SNACK 4
+
+/* Consegna
+
+    Calcola la somma e la media dei numeri da 2 a 10
+
+*/
+
+int numbers = 10; 
+int sum = 0;
+
+for (int i = 2; i <= numbers; i++)
+{
+    sum = sum + i;
+}
+
+float median = (float)sum / (numbers - 2);
+
+Console.WriteLine($"La somma dei numeri da 2 a 10 è: {sum} , mentre la media è {median}");
+
 #endregion
