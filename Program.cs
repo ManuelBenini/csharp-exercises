ec97335b1e6be9c06b4b254044033a4cc060b365
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-16 14:27:28 +0200

    Snack 2 terminato

--- Program.cs
+++ Program.cs
@@ -21,3 +21,27 @@ else
     Console.WriteLine($"Il numero più grande è {secondNumber}, il più piccolo è {firstNumber}");
 }
 #endregion
+
+#region SNACK 2
+
+/* Consegna
+
+    L'utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la più lunga.
+
+*/
+
+Console.WriteLine("Inserire prima parola");
+string firstWord = Console.ReadLine();
+
+Console.WriteLine("Inserire seconda parola");
+string secondWord = Console.ReadLine();
+
+if (firstWord.Length > secondWord.Length)
+{
+    Console.WriteLine($"La parola più corta è {secondWord}, la più lunga è {firstWord}");
+}
+else
+{
+    Console.WriteLine($"La parola più corta è {firstWord}, la più lunga è {secondWord}");
+}
+#endregion
